720b818109daeeeafec42920fe62fadfb3ad6d01
Lakshmee Mohanan <lakshmee.mohan12@gmail.com>
2025-07-18 13:15:06 +0200

    titles updated

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <h2 class="text-center text-3xl mb-4 font-bold border border-gray-30 p-3">Welcome to Workopia</h1>
+    <h2 class="text-center text-3xl mb-4 font-bold border border-gray-30 p-3">Welcome to my Job Portal</h1>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             @forelse($jobs as $job)
                 <x-job-card :job="$job" />
